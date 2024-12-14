3653e42855d0d0d45632dc52f051f669d152ea6b
Cheeze06 <your_email@example.com>
2024-12-14 16:16:35 +0900

    2024-12-14 '열차 정보 가져오기' 버튼 ㄴAPI 내용 한글 깨짐 이슈 정상화

--- lib/src/train_code/API_return.dart
+++ lib/src/train_code/API_return.dart
@@ -1,15 +1,13 @@
+import 'dart:convert'; // utf8과 jsonDecode 사용
 import 'package:http/http.dart' as http;
-import 'package:charset_converter/charset_converter.dart'; // CP949 디코딩을 위한 패키지
 
 class TrainAPI {
   static const String apiUrl =
       "https://apis.data.go.kr/1613000/TrainInfoService/getStrtpntAlocFndTrainInfo";
-
-  // 주어진 URL 인코딩된 서비스 키를 여기에 넣으세요.
   static const String serviceKey =
       "om0pDKsS%2FyWxxcSKSNMvpg%2FtDfbr25yCx2FiManiEtKkDT8whQoehzUC0BMNcWvXQ50EdaB3p2RgpIol5frQ0w%3D%3D";
 
-  Future<String> fetchTrainInfo({
+  Future<dynamic> fetchTrainInfo({
     required String depPlaceId,
     required String arrPlaceId,
     required String depPlandTime,
@@ -18,15 +16,16 @@ class TrainAPI {
     int pageNo = 1,
   }) async {
     final String url =
-        '$apiUrl?serviceKey=$serviceKey&depPlaceId=$depPlaceId&arrPlaceId=$arrPlaceId&depPlandTime=$depPlandTime&trainGradeCode=$trainGradeCode&numOfRows=$numOfRows&pageNo=$pageNo&_type=json';
+        '$apiUrl?serviceKey=$serviceKey&depPlaceId=$depPlaceId&arrPlaceId=$arrPlaceId&depPlandTime=$depPlandTime'
+        '&trainGradeCode=$trainGradeCode&numOfRows=$numOfRows&pageNo=$pageNo&_type=json';
 
     try {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        // 바이트 데이터를 CP949로 디코딩
-        final decodedBody = await CharsetConverter.decode("euc-kr", response.bodyBytes);
-        return decodedBody; // 디코딩된 문자열 반환
+        // 한글 깨짐 방지: UTF-8로 디코딩 후 JSON 디코딩
+        final decodedBody = jsonDecode(utf8.decode(response.bodyBytes));
+        return decodedBody; // JSON 데이터 반환
       } else {
         return "API 호출 실패: ${response.statusCode}";
       }
